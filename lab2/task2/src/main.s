ad741157dc476fe8ea4528b237a7d836cc8afdc6
DmitryMustk <dmutrymust@gmail.com>
2024-03-04 16:55:53 +0700

    [lab2/taks2] add error checking

--- lab2/task2/src/main.s
+++ lab2/task2/src/main.s
@@ -1,23 +1,30 @@
 .section .data
-
 msg:
     .ascii "Hello world\n"
     len = . - msg
 
 .section .text
-
 .globl _start
 
 _start:
-    #write
-    mov $4, %eax         
-    mov $1, %ebx         
+    # write
+    mov $4, %eax         # write code to eax (4)
+    mov $1, %ebx         # stdout fd to ebx (1)
     mov $msg, %ecx       
     mov $len, %edx       
     int $0x80            
-    
-    mov %eax, %ebx       
 
-    #exit
+    #is_write_correct
+    cmp $len, %eax       
+    jne write_failed     
+
+    
+    xor %ebx, %ebx         
+    jmp exit_program     
+
+write_failed:
+    mov $-1, %ebx       
+
+exit_program:
     mov $1, %eax         
-    int $0x80            
+    int $0x80           
